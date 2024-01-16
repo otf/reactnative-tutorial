5d5d870dc2d58f5dfe5cf6fd78a8d41c89dbe8bc
otf <otf@me.com>
2024-01-17 07:49:40 +0900

    Handling text input.

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
-import Cat from "./Cat";
+import PizzaTranslator from "./PizzaTranslator";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Cat />
+      <PizzaTranslator />
       <StatusBar style="auto" />
     </View>
   );
