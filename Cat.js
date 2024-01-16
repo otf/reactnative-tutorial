fb0a9166051e1843704cb5dadbccb5c5c07e2c22
otf <otf@me.com>
2024-01-16 10:33:04 +0900

    use a state for cat component.

--- Cat.js
+++ Cat.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import {Text, TextInput, View, Image} from 'react-native';
+import React, {useState} from 'react';
+import {Button, Text, TextInput, View, Image} from 'react-native';
 
 const Cat = props => {
+  const [isHungry, setIsHungry] = useState(true);
   return (
     <View>
+      <Text>
+        I am {props.name}, and I am {isHungry ? 'hungry' : 'full'}!
+      </Text>
+      <Button
+        onPress={() => {
+          setIsHungry(false);
+        }}
+        disabled={!isHungry}
+        title={isHungry ? 'Pour me some milk, please!' : 'Thank you'}
+        />
       <Image
         source={{
           uri: 'https://reactnative.dev/docs/assets/p_cat1.png'
