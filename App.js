1436760e6253c42b7752b59fabe13b41e839f0e1
otf <otf@me.com>
2024-01-16 10:03:18 +0900

    add cat component

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Cat from './Cat';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Cat/>
       <StatusBar style="auto" />
     </View>
   );
