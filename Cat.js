d2188ceecbcf9c4b70c3d0cc646355d293ed4efc
otf <otf@me.com>
2024-01-16 10:04:55 +0900

    use a const variable.

--- Cat.js
+++ Cat.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {Text} from 'react-native';
 
 const Cat = () => {
-  return <Text>Hello, I am your cat!</Text>;
+  const name = 'Maru';
+  return <Text>Hello, I am {name}</Text>;
 };
 
 export default Cat;
