9680bc8d7cc1eab8f1d4ba181db8189dd9034933
otf <otf@me.com>
2024-01-17 23:27:32 +0900

    Use ScrollView.

--- App.js
+++ App.js
@@ -1,22 +1,46 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, Text } from "react-native";
 
-import PizzaTranslator from "./PizzaTranslator";
+const logo = {
+  uri: "https://reactnative.dev/img/tiny_logo.png",
+  width: 64,
+  height: 64,
+};
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <PizzaTranslator />
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => (
+  <ScrollView>
+    <Text style={{ fontSize: 96 }}>Scroll me plz</Text>
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Text style={{ fontSize: 96 }}>If you like</Text>
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Text style={{ fontSize: 96 }}>Scrolling down</Text>
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Text style={{ fontSize: 96 }}>What's the best</Text>
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Text style={{ fontSize: 96 }}>Framework around?</Text>
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Image source={logo} />
+    <Text style={{ fontSize: 80 }}>React Native</Text>
+  </ScrollView>
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
